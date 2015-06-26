27cb366d403593ec76bc3c385f33c40651f6653d
bryan.local\davidh <david.hollins@centricconsulting.com>
2015-06-26 12:21:02 -0400

    Correct getting the drive when clicked from the driving log

--- app/src/main/java/com/centricconsulting/driversedtracker/drivinglog/DrivingLogFragment.java
+++ app/src/main/java/com/centricconsulting/driversedtracker/drivinglog/DrivingLogFragment.java
@@ -106,7 +106,8 @@ public class DrivingLogFragment extends Fragment implements AbsListView.OnItemCl
         if (null != mListener) {
             // Notify the active callbacks interface (the activity, if the
             // fragment is attached to one) that an item has been selected.
-            mListener.onDriveTap(mDriveRepository.findById(id));
+
+            mListener.onDriveTap((Drive)parent.getItemAtPosition(position));
         }
     }
 
